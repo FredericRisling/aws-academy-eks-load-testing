9d6f0ca85a08c037d4955e0375e2a56c4dccbaa8
risling <frederic.risling@adesso.de>
2023-01-08 15:35:30 +0100

    add dependencies to script

--- bench.sh
+++ bench.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 echo "Starting benchmark ..."
 
+
+# Installing Dependencies
+
+apt update > /dev/null 2>&1
+apt install default-jre sysbench docker.io iperf > /dev/null 2>&1
+
+
 # benchmark the disk read speed
 sysbench --file-test-mode=seqrd fileio prepare > /dev/null 2>&1
-DISK_WRITE = $(sysbench --file-test-mode=seqrd fileio run)
+DISK_READ=$(sysbench --file-test-mode=seqrd fileio run | grep 'read, MiB/s:')
+sysbench --file-test-mode=seqwr fileio prepare > /dev/null 2>&1
+DISK_WRITE=$(sysbench --file-test-mode=seqwr fileio run | grep 'written, MiB/s:')
+
+
+echo "disk read: " $DISK_READ
+echo "disk write: " $DISK_WRITE
+echo "Done benchmarking"
 
 
 
